439f608fcf8a006d26084041c64dd14c74874f35
Arkar Maung <49133212+rkarmaung@users.noreply.github.com>
2019-04-10 04:08:41 +0800

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,24 @@
+// Js for Navbar open & close
+var burgerBtn = document.getElementsByClassName("burgerBtn"),
+    burgerContent = document.getElementsByClassName("burgerContent");
+    x=0;
+    
+burgerBtn[0].addEventListener("click", function(){
+    if(x===0){
+	    burgerContent[0].style.transform = "perspective(1200px) rotateY(0deg)";
+	    burgerContent[0].style.transition = "all 1000ms ease";
+	    x++;
+    } else {
+	    burgerContent[0].style.transform = "perspective(1200px) rotateX(90deg)";
+	    x--;
+    }
+})
+
+
+
+// Close Navbar when click anywhere on window
+window.addEventListener("mouseup", function(e){
+    if(e.target != burgerContent[0]){
+	    burgerContent[0].style.transform = "perspective(1200px) rotateX(90deg)";
+    }
+})
